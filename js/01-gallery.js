7bf02a091b26bac18ccbd5c503dd5e97992b053b
realex3355 <realex3355@gmail.com>
2023-05-22 21:53:16 +0300

    1--1 part

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryPlace = document.querySelector(".gallery");
+
+const galleryItemsList = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  )
+  .join("");
+
+galleryPlace.insertAdjacentHTML("afterbegin", galleryItemsList);
+galleryPlace.addEventListener("click", (event) => {
+  console.log(event.target.dataset.source);
+});
+
+//console.log(galleryItems);
